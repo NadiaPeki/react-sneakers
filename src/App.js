617996cd1816414f50b79fcbbc155ca21c2e474e
Nadzeya Pekarskaya <napekarskaya@gmail.com>
2023-08-13 13:09:19 +0200

    Changed Card, App, make scss modules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Card from './components/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
 
+const arr = [
+  {
+    title: 'Nike Blazer Mid Suede',
+    price: 500,
+    imageUrl: '/img/sneakers/1.jpg',
+  },
+  {
+    title: 'Nike Air Max 270',
+    price: 555,
+    imageUrl: '/img/sneakers/2.jpg',
+  },
+  {
+    title: '550 White Pink Sea Salt',
+    price: 533,
+    imageUrl: '/img/sneakers/3.jpg',
+  },
+  {
+    title: '9060 Rain Cloud',
+    price: 566,
+    imageUrl: '/img/sneakers/4.jpg',
+  },
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,10 +40,9 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
         </div>
       </div>
     </div>
